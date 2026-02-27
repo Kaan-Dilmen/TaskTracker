c8765ecb945e813f393e7d02263b42f39073d587
Kaan Dilmen <lucakaan1@yahoo.co.uk>
2026-02-27 11:41:02 +0000

    basic input and add to list
    text field for task text, adds to list on button press.

--- FreshReact/fresh-react-project/src/App.jsx
+++ FreshReact/fresh-react-project/src/App.jsx
@@ -1,12 +1,36 @@
-//import { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  function handleAddTask() {
+    if (value.trim() === "") return;
+    setTasks([tasks, value]);
+    setValue("");
+  }
+
   return (
     <div className="container">
-      <div className ="box">
-        <h1>Hello React 🚀</h1>
+      <div className="box">
+        <h1>Task Tracker 🚀</h1>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          placeholder="Enter a task"
+        />
+
+        <button className="button" onClick={handleAddTask}>
+          Add Task
+        </button>
       </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
     </div>
   );
 }

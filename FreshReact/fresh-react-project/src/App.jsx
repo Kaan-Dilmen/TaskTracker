f9cd3468e3948678f0d4134606a5a849be3c6cf2
Kaan Dilmen <lucakaan1@yahoo.co.uk>
2026-03-01 16:06:02 +0000

    Editing and saving tasks
    Ability to add a task, edit and save that task.

--- FreshReact/fresh-react-project/src/App.jsx
+++ FreshReact/fresh-react-project/src/App.jsx
@@ -4,38 +4,55 @@ import "./App.css";
 function App() {
   const [value, setValue] = useState("");
   const [tasks, setTasks] = useState([]);
+  const [editValue, setEditValue] = useState("");
 
   function handleAddTask() {
     if (value.trim() === "") return;
 
-    setTasks([...tasks, 
+    setTasks([
+      ...tasks,
       {
         id: Date.now(),
         text: value,
         completed: false,
-        isEditing: false
-      }]);
+        isEditing: false,
+      },
+    ]);
 
     setValue("");
   }
 
-  function handleDelete(id){
-    setTasks(tasks.filter(task => task.id !== id));
+  function handleDelete(id) {
+    setTasks(tasks.filter((task) => task.id !== id));
   }
 
-  function handleCompleted(id){
+  function handleCompleted(id) {
     setTasks(
-      tasks.map(task => 
-        task.id === id? {...task, completed : !task.completed} : task
-      )
+      tasks.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task,
+      ),
     );
   }
 
-  function handleEditing(id){
+  function handleEditing(id) {
+    const taskToEdit = tasks.find((task) => task.id === id);
+
+    if (!taskToEdit.isEditing) {
+      setEditValue(taskToEdit.text);
+    }
+
     setTasks(
-      tasks.map(task =>
-        task.id === id? {...task, isEditing : !task.isEditing} : task
-      )
+      tasks.map((task) =>
+        task.id === id ? { ...task, isEditing: !task.isEditing } : task,
+      ),
+    );
+  }
+
+  function handleSave(id) {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, text: editValue, isEditing: false } : task,
+      ),
     );
   }
 
@@ -58,31 +75,50 @@ function App() {
         <button className="button" onClick={handleAddTask}>
           Add Task
         </button>
-      </div>      
+      </div>
 
       <div className="listContainer">
         {/*List*/}
         <ul>
           {tasks.map((task) => (
             <li key={task.id}>
-              {task.text}
+              {task.isEditing ? (
+                <input
+                  value={editValue}
+                  onChange={(e) => setEditValue(e.target.value)}
+                />
+              ) : (
+                task.text
+              )}
               {/*Mark Complete Button*/}
-              <button className={task.completed ? "completed" : "button"} onClick={() => handleCompleted(task.id)}>
-              Mark Complete
-            </button>
-            {/*Delete Button*/}
-            <button className="button" onClick={() => handleDelete(task.id)}>
-              Delete
-            </button>
-            {/*Edit Button*/}
-            <button className="button" onClick={() => handleEditing(task.id)}>
-              Edit Task
-            </button>
+              <button
+                className={task.completed ? "completed" : "button"}
+                onClick={() => handleCompleted(task.id)}
+              >
+                Mark Complete
+              </button>
+              {/*Delete Button*/}
+              <button className="button" onClick={() => handleDelete(task.id)}>
+                Delete
+              </button>
+              {/* Edit / Save Button */}
+              {task.isEditing ? (
+                <button className="button" onClick={() => handleSave(task.id)}>
+                  Save Task
+                </button>
+              ) : (
+                <button
+                  className="button"
+                  onClick={() => handleEditing(task.id)}
+                >
+                  Edit Task
+                </button>
+              )}
             </li>
           ))}
         </ul>
       </div>
-    </div>    
+    </div>
   );
 }
 
